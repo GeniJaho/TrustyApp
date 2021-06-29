8e4352f70b6d5540648a1e617efbe91572998fb3
nafismuhymeen <muhymeennafis@gmail.com>
2021-06-29 20:02:56 +0600

    Registration and Login Complete

--- api/src/Application/Actions/Review/ReviewAction.php
+++ api/src/Application/Actions/Review/ReviewAction.php
@@ -4,34 +4,36 @@ declare(strict_types=1);
 namespace App\Application\Actions\Review;
 
 use App\Application\Actions\Action;
+use App\Application\Actions\AuthorizesUsers;
 use App\Domain\Review\ReviewRepository;
 use App\Domain\User\CraftsmanRepository;
+use App\Domain\User\UserRepository;
 use Psr\Log\LoggerInterface;
 
 abstract class ReviewAction extends Action
 {
-    /**
-     * @var ReviewRepository
-     */
-    protected $reviewRepository;
-    /**
-     * @var CraftsmanRepository
-     */
-    protected $craftsmanRepository;
+    use AuthorizesUsers;
+
+    protected ReviewRepository $reviewRepository;
+    protected CraftsmanRepository $craftsmanRepository;
+    protected UserRepository $userRepository;
 
     /**
      * @param LoggerInterface $logger
      * @param ReviewRepository $reviewRepository
      * @param CraftsmanRepository $craftsmanRepository
+     * @param UserRepository $userRepository
      */
     public function __construct(
         LoggerInterface $logger,
         ReviewRepository $reviewRepository,
-        CraftsmanRepository $craftsmanRepository
+        CraftsmanRepository $craftsmanRepository,
+        UserRepository $userRepository
     )
     {
         parent::__construct($logger);
         $this->reviewRepository = $reviewRepository;
         $this->craftsmanRepository = $craftsmanRepository;
+        $this->userRepository = $userRepository;
     }
 }
